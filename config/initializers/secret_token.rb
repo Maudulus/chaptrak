81e9e274e918fb88423cb759e986b481d0f35d7f
Modsky1 <maude.jake@gmail.com>
2014-03-20 16:35:03 -0400

    chaptrak 9000 up and running

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chaptrak::Application.config.secret_key_base = 'b1e0691d03fc53170454e7079567e4ed89435f8f2f55856085eb359d157232c71878f3840aa1fdc59278b75fec72a926d999cf6e61c0f38450172122d3687c32'
